4698cb5642e1a871f07bce560bf3844991420d07
Md Emamudin <mdemamudin726@gmail.com>
2025-10-16 15:24:25 +0530

    analytics fix

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,13 +1,61 @@
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// const TRACKING_ID = "G-7R741YLGY4";
+
+// function AnalyticsTracker() {
+//   const location = useLocation();
+
+//   useEffect(() => {
+
+//     const getDeviceType = () => {
+//       const ua = navigator.userAgent;
+//       if (/mobile/i.test(ua)) return "Mobile";
+//       if (/tablet/i.test(ua)) return "Tablet";
+//       return "Desktop";
+//     };
+
+//     ReactGA.initialize(TRACKING_ID);
+
+//     // Track pageview
+//     ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+
+//     // Track device type
+//     ReactGA.event({
+//       category: "User Info",
+//       action: "Device Type",
+//       label: getDeviceType(),
+//     });
+
+//     // Track approximate location
+//     fetch("https://ipapi.co/json/")
+//       .then(res => res.json())
+//       .then(data => {
+//         ReactGA.event({
+//           category: "User Info",
+//           action: "User Location",
+//           label: `${data.city}, ${data.country_name}`,
+//         });
+//       })
+//       .catch(() => {});
+
+//   }, [location]);
+
+//   return null;
+// }
+
+// export default AnalyticsTracker;
+
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 const TRACKING_ID = "G-7R741YLGY4";
 
 function AnalyticsTracker() {
-  const location = useLocation();
-
   useEffect(() => {
+   
+    if (typeof window === "undefined") return;
 
     const getDeviceType = () => {
       const ua = navigator.userAgent;
@@ -19,7 +67,7 @@ function AnalyticsTracker() {
     ReactGA.initialize(TRACKING_ID);
 
     // Track pageview
-    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 
     // Track device type
     ReactGA.event({
@@ -30,8 +78,8 @@ function AnalyticsTracker() {
 
     // Track approximate location
     fetch("https://ipapi.co/json/")
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         ReactGA.event({
           category: "User Info",
           action: "User Location",
@@ -39,10 +87,10 @@ function AnalyticsTracker() {
         });
       })
       .catch(() => {});
-
-  }, [location]);
+  }, []);
 
   return null;
 }
 
 export default AnalyticsTracker;
+
