dd626af31738018969ad912c3b6bef8942f8c457
Md Emamudin <mdemamudin726@gmail.com>
2025-10-16 14:54:03 +0530

    fixed build issue

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -4,41 +4,42 @@ import { useLocation } from "react-router-dom";
 
 const TRACKING_ID = "G-7R741YLGY4";
 
-function getDeviceType() {
-  const ua = navigator.userAgent;
-  if (/mobile/i.test(ua)) return "Mobile";
-  if (/tablet/i.test(ua)) return "Tablet";
-  return "Desktop";
-}
-
-ReactGA.initialize(TRACKING_ID);
-
 function AnalyticsTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    
+
+    const getDeviceType = () => {
+      const ua = navigator.userAgent;
+      if (/mobile/i.test(ua)) return "Mobile";
+      if (/tablet/i.test(ua)) return "Tablet";
+      return "Desktop";
+    };
+
+    ReactGA.initialize(TRACKING_ID);
+
     // Track pageview
     ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
 
     // Track device type
     ReactGA.event({
-    category: "User Info",
-    action: "Device Type",
-    label: getDeviceType(),
+      category: "User Info",
+      action: "Device Type",
+      label: getDeviceType(),
     });
 
     // Track approximate location
     fetch("https://ipapi.co/json/")
-    .then(res => res.json())
-    .then(data => {
+      .then(res => res.json())
+      .then(data => {
         ReactGA.event({
-        category: "User Info",
-        action: "User Location",
-        label: `${data.city}, ${data.country_name}`,
+          category: "User Info",
+          action: "User Location",
+          label: `${data.city}, ${data.country_name}`,
         });
-    })
-    .catch(() => {});
+      })
+      .catch(() => {});
+
   }, [location]);
 
   return null;
