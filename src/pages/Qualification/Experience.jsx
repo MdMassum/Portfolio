17a08d94db634f0696254b55c44ccb3e0fbb4c69
Md Emam <emam@Mds-MacBook-Air.local>
2026-02-12 00:48:37 +0530

    updated experience

--- src/pages/Qualification/Experience.jsx
+++ src/pages/Qualification/Experience.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 function Experience() {
   return (
@@ -8,8 +7,8 @@ function Experience() {
         {/* Data */}
         <div className="text-right -mt-1">
           <h4 className="font-semibold">Highway Delite</h4>
-          <p className="text-gray-600 dark:text-gray-300">Software Developer Intern</p>
-          <p className="text-gray-400 text-sm">Aug 2025 - Present</p>
+          <p className="text-gray-600 dark:text-gray-300">Software Developer</p>
+          <p className="text-gray-400 text-sm">Feb 2026 - Present</p>
         </div>
       </div>
       
@@ -33,25 +32,43 @@ function Experience() {
       <div className="flex justify-start border-l-2 border-gray-300 pl-4 -mx-0.5 pb-10">
         {/* Data */}
         <div>
-          <h4 className="font-semibold">Skillancer Private Limited</h4>
-          <p className="text-gray-600 dark:text-gray-300">FullStack Intern</p>
-          <p className="text-gray-400 text-sm">Mar 2025 - July 2025</p>
+          <h4 className="font-semibold">Highway Delite</h4>
+          <p className="text-gray-600 dark:text-gray-300">Software Developer (Internship)</p>
+          <p className="text-gray-400 text-sm">Aug 2025 - Jan 2026</p>
         </div>
       </div>
 
       {/* Third item - Right Side */}
-      <div className="flex justify-end border-r-2 border-gray-300 pr-4">
+      <div className="flex justify-end border-r-2 border-gray-300 pr-4 pb-10">
         {/* Data */}
         <div className="text-right">
-          <h4 className="font-semibold">Ricoz Private Limited</h4>
-          <p className="text-gray-600 dark:text-gray-300">FullStack Intern</p>
-          <p className="text-gray-400 text-sm">Sep 2024 - Feb - 2025</p>
+          <h4 className="font-semibold">Skillancer Private Limited</h4>
+          <p className="text-gray-600 dark:text-gray-300">FullStack Developer (Internship)</p>
+          <p className="text-gray-400 text-sm">Mar 2025 - July 2025</p>
         </div>
       </div>
       
       {/* Timeline Circle */}
       <div className="flex -mx-1.5 ">
         <div className="w-3 h-3 bg-black dark:bg-white rounded-full"></div>
+      </div>
+
+      <div></div>
+      <div></div>
+
+      {/* Timeline Circle */}
+      <div className="flex -mx-1.5 z-50 my-2">
+        <div className="w-3 h-3 bg-black dark:bg-white rounded-full"></div>
+      </div>
+
+      {/* Second item - Right Side */}
+      <div className="flex justify-start border-l-2 border-gray-300 pl-4 -mx-0.5 pb-10">
+        {/* Data */}
+        <div>
+          <h4 className="font-semibold">Ricoz Private Limited</h4>
+          <p className="text-gray-600 dark:text-gray-300">FullStack Developer (Internship)</p>
+          <p className="text-gray-400 text-sm">Sep 2024 - Feb - 2025</p>
+        </div>
       </div>
 
     </div>
